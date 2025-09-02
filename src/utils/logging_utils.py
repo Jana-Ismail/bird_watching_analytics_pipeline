e5a5911589cd516444c76b8f86212d69976afb38
Jana Ismail <jana.ismail.nss@gmail.com>
2025-09-02 14:52:43 -0500

    Add setup_logging utility function | Test console and file logging when level is set to DEBUG vs INFO

--- src/utils/logging_utils.py
+++ src/utils/logging_utils.py
@@ -1,4 +1,32 @@
-from datetime import datetime, timezone
+import logging
 
-def get_current_utc_timestamp(date_format_str):
-    return datetime.now(timezone.utc).strftime(date_format_str)
+from src.utils.file_utils import ensure_directory_exists
+from src.config.app_settings import LOG_DIR, LOG_FILE
+
+
+def setup_logger(name, log_file=LOG_FILE, level='INFO'):
+    """Set up a logger with file and console handlers"""
+    ensure_directory_exists(LOG_DIR)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    # Avoid duplicate handlers
+    if logger.handlers:
+        return logger
+    
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    if log_file:
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
