f29602c757ee7ee0b4648e69cff0b7178c36ac25
Jana Ismail <jana.ismail.nss@gmail.com>
2025-09-03 13:41:55 -0500

    Add get_recent_observations_by_region() function to ebird.py

---
+++ src/api_ingestion/ebird.py
@@ -0,0 +1,51 @@
+import os
+import requests
+
+from src.utils.logging_utils import setup_logger
+from src.config.app_settings import (
+    LOG_FILE,
+    EBIRD_API_KEY, 
+    EBIRD_OBSERVATION_BASE_URL
+)
+
+log_name = os.path.basename(__file__)
+logger = setup_logger(log_name, LOG_FILE)
+
+def get_recent_observations_by_region(region_code, base_url=EBIRD_OBSERVATION_BASE_URL, api_key=EBIRD_API_KEY, query_params=None):
+    logger.info(f'Fetching recent observations for region: "{region_code}"')
+    if query_params is None:
+        query_params = {}
+
+    headers = {
+        'X-ebirdApiToken': api_key
+    }
+
+    url = f'{base_url}/{region_code}/recent'
+
+    try:
+        response = requests.get(url, headers=headers, params=query_params)
+        response.raise_for_status()
+        logger.info(f'Successfully fetched data for region: "{region_code}" at url: {url} with query params: {query_params if query_params else "{}"}')
+    except requests.exceptions.RequestException as e:
+        logger.error(f'Error fetching data from eBird API: {e}')
+        raise
+
+    return response.json()
+
+
+def main():
+    logger.info('Starting eBird API ingestion')
+    
+    logger.info(f'Fetching eBird API recent observation data')
+    # build CO url
+    region_code = 'US-CO'
+    query_params = {
+        'maxResults': 10,
+        'back': 7
+    }
+    co_observations = get_recent_observations_by_region(region_code, query_params=query_params)
+
+
+
+if __name__ == "__main__":
+    main()
--- src/config/app_settings.py
+++ src/config/app_settings.py
@@ -1,6 +1,20 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 
+# Logging Credentials
 LOG_DIR = PROJECT_ROOT / 'logs'
 LOG_FILE = LOG_DIR / 'bird_watching_analytics_pipeline.log'
+
+# Ebird API Credentials
+EBIRD_API_KEY = os.getenv('EBIRD_API_KEY')
+EBIRD_OBSERVATION_BASE_URL = os.getenv('EBIRD_OBSERVATION_BASE_URL')
+
+EBIRD_REGION_CODE = 'US-CO'
+EBIRD_OBSERVATION_ENDPOINTS = {
+    "recent_by_region": f"{EBIRD_OBSERVATION_BASE_URL}/data/obs/{EBIRD_REGION_CODE}/recent",
+}
