63e070c4789ee89c9c4a041908abe1d25b09cf70
Jana Ismail <jana.ismail.nss@gmail.com>
2025-09-12 03:40:34 +0000

    Airflow DAG for daily ingestion of open-meteo weather data

--- airflow/dags/open_meteo_daily_dag.py
+++ airflow/dags/open_meteo_daily_dag.py
@@ -9,33 +9,31 @@ if DEV_CONTAINER_ROOT not in sys.path:
     sys.path.append(DEV_CONTAINER_ROOT)
 
 from src.utils.logging_utils import setup_logger, get_log_name
-# from src.utils.file_utils import LOG_FILE
 
 logger = setup_logger(get_log_name(__file__), '/opt/airflow/logs/ingestion_dag.log')
 
-
 try:
-    from src.api_ingestion.birds.ebird_daily import main as ebird_daily_main
+    from src.api_ingestion.weather.open_meteo_daily import main as open_meteo_daily_main
 except ImportError as e:
     logger.error(f'ERROR: Could not import external scripts. Check Airflow PYTHONPATH in Airflow containers.')
     logger.error(f'ImportError: {e}')
     raise
 
-def run_ebird_daily_ingestion(**kwargs):
+def run_open_meteo_ingestion(**kwargs):
     kwargs['ti'].log.info(f'Running ebird_daily_main for DAG run {kwargs["dag_run"].run_id}')
-    ebird_daily_main()
+    open_meteo_daily_main()
     kwargs['ti'].log.info(f'Completed ebird_daily_main for DAG run {kwargs["dag_run"].run_id}')
 
 with DAG(
-    dag_id='ebird_daily_ingestion_pipeline',
+    dag_id='open_meteo_daily_weather_ingestion_pipeline',
     start_date=days_ago(1),
     schedule_interval='0 17 * * *',
     catchup=False,
-    tags=['bird_sightings', 'daily_ingestion', 'ebird', 'api', 'raw', 'minio']
+    tags=['weather', 'general', 'daily_ingestion', 'open_meteo', 'api', 'raw', 'minio']
 ) as dag:
 
-    ebird_daily_api_to_minio_task = PythonOperator(
-        task_id='run_ebird_daily_ingestion',
-        python_callable=run_ebird_daily_ingestion,
+    open_meteo_daily_api_to_minio_task = PythonOperator(
+        task_id='run_open_meteo_daily_ingestion',
+        python_callable=run_open_meteo_ingestion,
         provide_context=True
     )
